30b95ac6dded6b5cf4f5faecf9f398d23e5afb85
Cristian Angulo Nova <cristianangulonova@gmail.com>
2015-04-18 14:09:51 -0500

    Enrutamiento de Quiz

--- src/AppBundle/Controller/Admin/QuizController.php
+++ src/AppBundle/Controller/Admin/QuizController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Quiz\QuizBundle\Controller;
+namespace AppBundle\Controller\Admin;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
