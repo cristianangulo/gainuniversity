9fe3e66a4cbe24df764bd10493e660678522332b
Cristian Angulo Nova <cristianangulonova@gmail.com>
2015-06-11 16:16:26 -0500

    PDF

---
+++ src/AppBundle/Controller/Front/ItemPDFController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Controller\Front;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ItemPDFController extends Controller
+{
+
+    public function itemPDFAction($curso, $modulo, $item)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        if(!$this->get('app.usuario_curso')->usuarioCursoModuloItem($curso, $modulo, $item)){
+            $this->get('app.mensajero')->add('info','Usted no tiene acceso a los recursos que solicita');
+
+            return $this->redirect($this->generateUrl('front_perfil'));
+        }
+
+        $curso  = $this->get('app.model.cursos')->curso($curso);
+        $modulo = $this->get('app.model.modulos')->modulo($modulo);
+        $item   = $this->get('app.model.items')->item($item);
+
+        return $this->render('Front/items/item-pdf.html.twig', array(
+          'curso'   => $curso,
+          'modulo'  => $modulo,
+          'item'    => $item,
+          'item_id' => $item->getId()
+        ));
+
+    }
+
+}
+
+
+?>
